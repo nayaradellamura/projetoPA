3ca37f87c3be1510de0c1411dfef36366dc6b67e
Nayara Dellamura <168658191+nayaradellamura@users.noreply.github.com>
2025-04-12 22:07:48 -0300

    alterando login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const Login = () => {
     const [email, setEmail] = useState("");
@@ -7,20 +7,27 @@ export const Login = () => {
     const handleEntrar = () => {
        console.log(email)
        console.log(senha)
-
-
     }
+
+    useEffect(() => {
+        console.log(email)
+      },[email]);
+
+      useEffect(() => {
+       console.log(senha)
+      },[senha]);
+
     return (
         <div>
             <form>
                 <label>
                     <span>Email</span>
-                    <input/>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type="senha"/>
+                    <input type="password" value={senha} onChange={e => setSenha (e.target.value)}/>
                 </label>
 
                 <button type="button" onClick={handleEntrar}>Entrar</button>
